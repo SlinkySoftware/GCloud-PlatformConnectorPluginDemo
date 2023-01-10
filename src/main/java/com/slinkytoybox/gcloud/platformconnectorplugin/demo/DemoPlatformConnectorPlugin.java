0c304863dc9003a0964c630de644017827f7ef78
Michael Junek <michael@juneks.com.au>
2023-01-10 17:03:13 +1100

    Shell classes to do some work

--- src/main/java/com/slinkytoybox/gcloud/platformconnectorplugin/demo/DemoPlatformConnectorPlugin.java
+++ src/main/java/com/slinkytoybox/gcloud/platformconnectorplugin/demo/DemoPlatformConnectorPlugin.java
@@ -23,6 +23,7 @@ import com.slinkytoybox.gcloud.platformconnectorplugin.PlatformConnectorPlugin;
 import com.slinkytoybox.gcloud.platformconnectorplugin.request.*;
 import com.slinkytoybox.gcloud.platformconnectorplugin.response.*;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  *
@@ -31,6 +32,24 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class DemoPlatformConnectorPlugin implements PlatformConnectorPlugin {
 
+    @Value("info.build.version")
+    public String pluginVersion;
+
+    @Value("info.build.name")
+    public String pluginName;
+
+    
+    @Override
+    public String getPluginVersion() {
+        return pluginVersion;
+    }
+    
+    @Override
+    public String getPluginName() {
+        return pluginName;
+    }
+    
+       
     @Override
     public PluginResponse getResponseFromRequest(PluginRequest request) {
         final String logPrefix = "getResponseFromRequest() - ";
@@ -39,44 +58,88 @@ public class DemoPlatformConnectorPlugin implements PlatformConnectorPlugin {
 
         if (request.getClass().isAssignableFrom(CreateRequest.class)) {
             log.debug("{} - Create Request", logPrefix);
-            return doWork((CreateRequest)request);
+            return doWork((CreateRequest) request);
         }
         if (request.getClass().isAssignableFrom(UpdateRequest.class)) {
             log.debug("{} - Create Request", logPrefix);
-            return doWork((UpdateRequest)request);
+            return doWork((UpdateRequest) request);
         }
         if (request.getClass().isAssignableFrom(ReadRequest.class)) {
             log.debug("{} - Create Request", logPrefix);
-            return doWork((ReadRequest)request);
+            return doWork((ReadRequest) request);
         }
         if (request.getClass().isAssignableFrom(DeleteRequest.class)) {
             log.debug("{} - Create Request", logPrefix);
-            return doWork((DeleteRequest)request);
+            return doWork((DeleteRequest) request);
         }
-        
+
         log.error("{}Request class type not implemented", logPrefix);
         throw new IllegalArgumentException("Request class type not implemented");
     }
-    
+
     private CreateResponse doWork(CreateRequest req) {
-        return null;
+        final String logPrefix = "doWork(CreateRequest) - ";
+        log.trace("{}Entering Method", logPrefix);
         
+        log.info("{}Issuing create request for new record", logPrefix);
+        CreateResponse response = new CreateResponse();
+        response.setRequestId(req.getRequestId());
+        response.setSuccess(Boolean.TRUE);
+        response.setObjectId("DemoCreateObjectId");
+
+        log.debug("{}Returning response: {}", logPrefix, response);
+        return response;
+
     }
-    
+
     private UpdateResponse doWork(UpdateRequest req) {
-        return null;
+        final String logPrefix = "doWork(UpdateRequest) - ";
+        log.trace("{}Entering Method", logPrefix);
         
+        log.info("{}Issuing update request for record {}", logPrefix, req.getObjectId());
+        UpdateResponse response = new UpdateResponse();
+        response.setRequestId(req.getRequestId());
+        response.setSuccess(Boolean.TRUE);
+        response.setObjectId(req.getObjectId());
+
+        log.debug("{}Returning response: {}", logPrefix, response);
+        return response;
+
     }
 
     private ReadResponse doWork(ReadRequest req) {
-        return null;
+        final String logPrefix = "doWork(ReadRequest) - ";
+        log.trace("{}Entering Method", logPrefix);
         
+        if (req.getObjectId() != null || !req.getObjectId().isEmpty()) {
+            log.info("{}Issuing read request for record {}", logPrefix, req.getObjectId());
+        } else {
+            log.info("{}Issuing read request with search parameters", logPrefix);
+        }
+
+        ReadResponse response = new ReadResponse();
+        response.setRequestId(req.getRequestId());
+        response.setSuccess(Boolean.TRUE);
+        response.setObjectId("DemoReadObjectId");
+
+        log.debug("{}Returning response: {}", logPrefix, response);
+        return response;
+
     }
 
     private DeleteResponse doWork(DeleteRequest req) {
-        return null;
+        final String logPrefix = "doWork(DeleteRequest) - [" + req.getRequestId() + "]";
+        log.trace("{}Entering Method", logPrefix);
         
+        log.info("{}Issuing delete request for record {}", logPrefix, req.getObjectId());
+        DeleteResponse response = new DeleteResponse();
+        response.setRequestId(req.getRequestId());
+        response.setSuccess(Boolean.TRUE);
+        response.setObjectId(req.getObjectId());
+
+        log.debug("{}Returning response: {}", logPrefix, response);
+        return response;
+
     }
-            
-    
+
 }
