357729b1e83447bc6a0e4821cad46fe8ce0b11cd
Michael Junek <michael@juneks.com.au>
2023-01-13 13:45:27 +1100

    Configuration pushed through, start/stop operational, base framework in place

---
+++ src/main/java/com/slinkytoybox/gcloud/platformconnectorplugin/demo/DemoPluginWorker.java
@@ -0,0 +1,252 @@
+/*
+ *   platformconnectorplugindemo - DemoPluginWorker.java
+ *
+ *   Copyright (c) 2022-2023, Slinky Software
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as
+ *   published by the Free Software Foundation, either version 3 of the
+ *   License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   A copy of the GNU Affero General Public License is located in the 
+ *   AGPL-3.0.md supplied with the source code.
+ *
+ */
+package com.slinkytoybox.gcloud.platformconnectorplugin.demo;
+
+import com.slinkytoybox.gcloud.platformconnectorplugin.PlatformConnectorPlugin;
+import com.slinkytoybox.gcloud.platformconnectorplugin.request.*;
+import com.slinkytoybox.gcloud.platformconnectorplugin.response.*;
+import java.util.Properties;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * This class is the main worker for the plugin. It must implement the
+ * PlatformConnectorPlugin interface which defines the tasks that can be called
+ * from within the main application.
+ * The class is instantiated when the plugin is started, and destroyed when the
+ * plugin is stopped.
+ * 6 functions need to be customised, pluginSetup, pluginDestroy, and the doWork
+ * routines with four different signatures.
+ * 
+ * 
+ * @author Michael Junek (michael@juneks.com.au)
+ *
+ */
+@Slf4j
+@Component
+public class DemoPluginWorker implements PlatformConnectorPlugin {
+
+    private final Properties config;
+    private final String pluginId;
+    private final String pluginDescription;
+
+
+
+    // Custom Setup Routine
+    private void pluginSetup() {
+        final String logPrefix = "pluginSetup() - ";
+        log.trace("{}Entering Method", logPrefix);
+        // TODO: Any Setup work in here
+        
+    }
+    
+    
+    // Custom destruction routine
+    private void pluginDestroy() {
+        final String logPrefix = "pluginDestroy() - ";
+        log.trace("{}Entering Method", logPrefix);
+        // TODO: Any Setup work in here
+        
+    }
+
+    
+    /**
+     * Method for Creating a new object
+     *
+     * @param req
+     * @return
+     */
+    private CreateResponse doWork(CreateRequest req) {
+        final String logPrefix = "doWork(CreateRequest) - ";
+        log.trace("{}Entering Method", logPrefix);
+        log.info("{}Issuing create request for new record", logPrefix);
+        CreateResponse response = new CreateResponse(); // Create new response object
+        
+        // TODO: Modify code here to do the actual work
+        
+        response.setRequestId(req.getRequestId());
+        response.setSuccess(Boolean.TRUE);
+        response.setObjectId("DemoCreateObjectId");
+        
+        // END actual work code
+        
+        log.debug("{}Returning response: {}", logPrefix, response);
+        return response;
+
+    }
+
+    /**
+     * Method for Updating an existing object
+     *
+     * @param req
+     * @return
+     */
+    private UpdateResponse doWork(UpdateRequest req) {
+        final String logPrefix = "doWork(UpdateRequest) - ";
+        log.trace("{}Entering Method", logPrefix);
+        log.info("{}Issuing update request for record {}", logPrefix, req.getObjectId());
+        UpdateResponse response = new UpdateResponse(); // Create new response object
+        
+        // TODO: Modify code here to do the actual work
+        
+        response.setRequestId(req.getRequestId());
+        response.setSuccess(Boolean.TRUE);
+        response.setObjectId(req.getObjectId());
+
+        // END actual work code
+        
+        log.debug("{}Returning response: {}", logPrefix, response);
+        return response;
+
+    }
+
+    /**
+     * Method for searching for and reading an object
+     *
+     * @param req
+     * @return
+     */
+    private ReadResponse doWork(ReadRequest req) {
+        final String logPrefix = "doWork(ReadRequest) - ";
+        log.trace("{}Entering Method", logPrefix);
+
+        if (req.getObjectId() != null || !req.getObjectId().isEmpty()) {
+            log.info("{}Issuing read request for record {}", logPrefix, req.getObjectId());
+        }
+        else {
+            log.info("{}Issuing read request with search parameters", logPrefix);
+        }
+
+        ReadResponse response = new ReadResponse(); // Create new response object
+        
+        // TODO: Modify code here to do the actual work
+        
+        response.setRequestId(req.getRequestId());
+        response.setSuccess(Boolean.TRUE);
+        response.setObjectId("DemoReadObjectId");
+
+        // END actual work code
+        
+        log.debug("{}Returning response: {}", logPrefix, response);
+        return response;
+
+    }
+
+    /**
+     * Method for Deleting an object
+     *
+     * @param req
+     * @return
+     */
+    private DeleteResponse doWork(DeleteRequest req) {
+        final String logPrefix = "doWork(DeleteRequest) - [" + req.getRequestId() + "]";
+        log.trace("{}Entering Method", logPrefix);
+        log.info("{}Issuing delete request for record {}", logPrefix, req.getObjectId());
+        DeleteResponse response = new DeleteResponse(); // Create new response object
+        
+        // TODO: Modify code here to do the actual work
+        
+        response.setRequestId(req.getRequestId());
+        response.setSuccess(Boolean.TRUE);
+        response.setObjectId(req.getObjectId());
+        
+        // END actual work code
+        
+        log.debug("{}Returning response: {}", logPrefix, response);
+        return response;
+
+    }
+
+/* 
+////////
+//////// NO NEED TO MODIFY ANYTHING DOWN HERE
+////////
+*/
+    
+   // Default CTOR called by instantiator
+    public DemoPluginWorker(String pluginId, String pluginDescription, Properties config) {
+        this.config = config;
+        this.pluginDescription = pluginDescription;
+        this.pluginId = pluginId;
+    }
+    
+    // Initial post-construction routine - don't need to modify this, it calls the custom one
+    @PostConstruct
+    private void setup() {
+        final String logPrefix = "setup() - ";
+        log.trace("{}Entering Method", logPrefix);
+        log.info("----------------------------------------------------------------------------");
+        log.info("{}Startup tasks for plugin running", logPrefix);
+        log.info("Plugin Id: {}", pluginId);
+        log.info("Decription: {}", pluginDescription);
+        log.info("----------------------------------------------------------------------------");
+        log.info("{}Configuration", logPrefix);
+        log.info("{}", config);
+        log.info("----------------------------------------------------------------------------");
+        pluginSetup();
+        
+        log.trace("{}Leaving Method", logPrefix);
+    }
+
+    // Initial pre-destruction routine - don't need to modify this, it calls the custom one
+    @PreDestroy
+    private void destroy() {
+        final String logPrefix = "destroy() - ";
+        log.trace("{}Entering Method", logPrefix);
+        log.info("{}Shutdown tasks for plugin running", logPrefix);
+        pluginDestroy();
+        log.trace("{}Leaving Method", logPrefix);
+
+    }
+    
+    
+    // Default routine to break the incoming request into four different work types depending on class instance
+    @Override
+    public PluginResponse getResponseFromRequest(PluginRequest request) {
+        final String logPrefix = "getResponseFromRequest() - ";
+        log.trace("{}Entering Method", logPrefix);
+        log.debug("{}Determining request type from class type: {}", logPrefix, request.getClass().getName());
+
+        if (request instanceof CreateRequest createRequest) {
+            log.debug("{} - Create Request", logPrefix);
+            return doWork(createRequest);
+        }
+        if (request instanceof UpdateRequest updateRequest) {
+            log.debug("{} - Update Request", logPrefix);
+            return doWork(updateRequest);
+        }
+        if (request instanceof ReadRequest readRequest) {
+            log.debug("{} - Read Request", logPrefix);
+            return doWork(readRequest);
+        }
+        if (request instanceof DeleteRequest deleteRequest) {
+            log.debug("{} - Delete Request", logPrefix);
+            return doWork(deleteRequest);
+        }
+
+        log.error("{}Request class type not implemented", logPrefix);
+        throw new IllegalArgumentException("Request class type not implemented");
+    }
+
+
+
+}
---
+++ src/main/java/com/slinkytoybox/gcloud/platformconnectorplugin/demo/PluginConfiguration.java
@@ -0,0 +1,77 @@
+/*
+ *   platformconnectorplugindemo - PluginConfiguration.java
+ *
+ *   Copyright (c) 2022-2023, Slinky Software
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as
+ *   published by the Free Software Foundation, either version 3 of the
+ *   License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   A copy of the GNU Affero General Public License is located in the 
+ *   AGPL-3.0.md supplied with the source code.
+ *
+ */
+package com.slinkytoybox.gcloud.platformconnectorplugin.demo;
+
+import com.slinkytoybox.gcloud.platformconnectorplugin.PlatformConnectorPlugin;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author Michael Junek (michael@juneks.com.au)
+ */
+@Configuration
+@Slf4j
+public class PluginConfiguration {
+
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Bean
+    public PlatformConnectorPlugin pluginInterface() {
+        final String logPrefix = "pluginInterface() - ";
+        log.trace("{}Entering Method", logPrefix);
+
+        // Get the plugin Id and description from the context
+        String pluginId = context.getId();
+        String pluginDescription = context.getDisplayName();
+
+        // Create a new properties class
+        Properties pluginProperties = new Properties();
+        
+        log.info("{}Reading configuration for plugin", logPrefix);
+        File currentJavaJarFile = new File(PluginConfiguration.class.getProtectionDomain().getCodeSource().getLocation().getPath());   
+String currentJavaJarFilePath = currentJavaJarFile.getAbsolutePath();
+String currentRootDirectoryPath = currentJavaJarFilePath.replace(currentJavaJarFile.getName(), "");
+        
+        
+        String configPath = currentRootDirectoryPath + pluginId + ".properties";
+        log.trace("{}-- Scanning {}", logPrefix, configPath);
+        try {
+            pluginProperties.load(new FileInputStream(configPath));
+        }
+        catch (IOException ex)  {
+            log.error("{}IOException encountered when opening config file. It may not exist", logPrefix, ex);
+        }
+        
+        // Create the new worker with its name, description and configuration
+        log.debug("{}Creating new plugin worker", logPrefix);
+        return new DemoPluginWorker(pluginId, pluginDescription, pluginProperties);
+    }
+
+}
